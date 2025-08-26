bfcc2bb594c9aecbe44b1d9f15859ce1848e878a
Siddhant khot <khotsiddhant1008@gmail.com>
2025-08-26 10:35:20 +0530

    lodash dependency added

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -7,8 +7,7 @@ const router = express.Router();
 // All routes are protected
 router.use(protect);
 
-router.route('/')
-  .get(getUsers);
+router.route('/').get(getUsers);
 
 router.route('/profile')
   .put(updateUserProfile);
